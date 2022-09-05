9213b5747f5d77124fba2345a124547b67e677b4
dim50dim <zheliezkov@gmail.com>
2022-09-05 16:35:31 +0300

    ignore

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function a(b){
+    // document.querySelector('.out').textContent = b * 6;
+    return b * 5;
+}
+console.log(a(5));
+console.log(a(5));
+let d = 9;
+function c(b){
+    // document.querySelector('.out').textContent = b * 6;
+    return b * d;
+}
